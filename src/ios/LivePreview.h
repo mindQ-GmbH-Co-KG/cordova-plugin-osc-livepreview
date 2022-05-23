099bfec52153e95fd94b9e421603b2e9da9afa6b
Cornelius000 <cornelius.mendrina@mindq.de>
2022-05-23 15:01:31 +0200

    ios test

---
+++ src/ios/LivePreview.h
@@ -0,0 +1,9 @@
+/********* LivePreview.h Cordova Plugin Header *******/
+
+#import <Cordova/LivePreview.h>
+
+@interface LivePreview : CDVPlugin
+
+- (void)echo:(CDVInvokedUrlCommand*)command;
+
+@end
